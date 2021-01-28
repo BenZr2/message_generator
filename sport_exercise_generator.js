f1ab30693f927573263bda1ecfa0f0799bf08e90
Ben Reitz <ben.reitz2@googlemail.com>
2021-01-28 18:38:56 +0100

    added repetition exercises and hold exercises and a seconds array

---
+++ sport_exercise_generator.js
@@ -0,0 +1,13 @@
+/* 
+This is going to be a programm which 
+generates random fitness exercises
+*/
+
+
+
+
+const repExercises = ['Push Ups', 'Pull Ups', 'Sit Ups', 'Russian Twists', 'Squats', 'Mountain Climbers', 'Lunges'];
+
+const holdExercises = ['Plank', 'Side Plank', 'Deep Squat', 'Superman', 'Handstand',];
+
+const seconds = [15, 30, 45, 60];
