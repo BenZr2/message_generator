a1426163354036be65c1aaed54087e80d8f99014
Ben Reitz <ben.reitz2@googlemail.com>
2021-01-28 19:11:07 +0100

    added generator for exercise and repitions and printed out the message

--- sport_exercise_generator.js
+++ sport_exercise_generator.js
@@ -12,3 +12,8 @@ const holdExercises = ['Plank', 'Side Plank', 'Deep Squat', 'Superman', 'Handsta
 
 const seconds = [15, 30, 45, 60];
 
+const exercise = repExercises[Math.floor(Math.random() * repExercises.length)];
+const repitions = Math.floor(Math.random() * 20);
+
+console.log(`You have to do ${repitions} ${exercise}! Now!`);
+
